6564256c7677a5d67ec2227280ac9dfcb723eed4
naufalafles <zamrinaufalcode@gmail.com>
2023-07-02 00:20:07 +1000

    Programmed travalay.js to list flight prices

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
-# READ ME
-## Hackathon 2023
-### Plan: 
-1. Augmented Reality Tour Guide
-2. Luggage Transportation between Hotels
-3. Sleep deprived Face Recognition for Driving
-4. Travel Expenses
-5. Traveling expenses connected to bank
+# Travalay
+#### Travalay, an app that gives you cheapest return flight prices and dates list from anywhere to everywhere.
+[Travalay](#travalay)
+- [Objectives](#a-objectives)
+- [Javascript](#b-javascript)
+- [Machines/Technologies](#c-machinestechnologies)
 
+## A. Objectives
+The work put in will achieve the standard objectives:
+* Create a program using javascript.
+* Program list sorted from the highest price to lowest in javascript.
+* Using amadeus api to search for flight prices.
+* "Real-world" programming integrated with git and GitHub.
+* Managing files according to actual working folders.
+  * work, playGround, projects, study, tests, etc.
+  * These folders should have their actual files.
+##
 
-## Travel Expenses
-### Tasks
-- Using Python as expense tracker or budgeting tools.
-### Assumptions
+## B. Javascript
+- Install npm packages: node-fetch version 3.3.1
+- FETCH method
+- Object array to struct list
+##
+
+## C. Machines/Technologies
+List of technologies used to achieve project's objectives:
+- macOS Ventura version 13.3
+- node.js
+- vscode
+- TigerVNC Viewer version 1.13.0
+- amadeus api
+- git
+- GitHub
+##
---
+++ src/travalay.js
@@ -0,0 +1,65 @@
+import fetch from 'node-fetch';
+
+const clientId = 'JY2j6X3sGxa8v5FP2AZ8YYPwqIkfpRXn';
+const clientSecret = 'g3AGZ2UdxRApulhY';
+const origin = 'SYD';
+
+// Get access token for authentication
+const tokenUrl = 'https://test.api.amadeus.com/v1/security/oauth2/token';
+const authData = new URLSearchParams();
+authData.append('grant_type', 'client_credentials');
+authData.append('client_id', clientId);
+authData.append('client_secret', clientSecret);
+
+fetch(tokenUrl, {
+    method: 'POST',
+    body: authData,
+})
+    .then(response => response.json())
+    .then(data => {
+        const accessToken = data.access_token;
+        // Call the Flight Inspiration Search API
+        const apiUrl = `https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=${origin}`;
+        const headers = {
+            'Authorization': `Bearer ${accessToken}`,
+        };
+
+        return fetch(apiUrl, { headers });
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Process the flight data and find the cheapest flight and date
+        const cheapestFlight = findCheapestFlight(data);
+        console.log(cheapestFlight);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+function findCheapestFlight(flightData) {
+    // Extract necessary information such as prices, dates, and destinations
+    // Find the cheapest flight and corresponding date
+    // Return an object with the cheapest flight details including date
+
+    // Example implementation: Assuming the response contains an array of destinations
+    const destinations = flightData.data;
+    let cheapestFlight = null;
+    const result = [];
+
+    destinations.forEach(destination => {
+        if (!cheapestFlight || destination.price.total >= cheapestFlight.price) {
+            cheapestFlight = { 
+                price: destination.price.total,
+                from: destination.origin,
+                to: destination.destination,
+                departureDate: destination.departureDate,
+                returnDate: destination.returnDate,
+            };
+            result.push(cheapestFlight);
+        }
+    });
+
+
+
+    return result;
+}
